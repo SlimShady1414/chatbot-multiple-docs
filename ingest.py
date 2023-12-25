0063da9029581d6200ad23b054a98b9c698c715b
AdhiNandanJ <adhinandanj@gmail.com>
2023-12-25 11:13:36 +0530

    updated ingest

--- ingest.py
+++ ingest.py
@@ -1,30 +1,32 @@
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader, PDFMinerLoader 
-from langchain.text_splitter import RecursiveCharacterTextSplitter 
-from langchain.embeddings import SentenceTransformerEmbeddings 
-from langchain.vectorstores import Chroma 
-import os 
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader, PDFMinerLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain.vectorstores import Chroma
+import os
 from constants import CHROMA_SETTINGS
+import streamlit as st
 
 persist_directory = "db"
+
+def data_ingestion(file_path):
+    loader = PDFMinerLoader(file_path)
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+    texts = text_splitter.split_documents(documents)
+    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+    db = Chroma.from_documents(texts, embeddings, persist_directory=persist_directory, client_settings=CHROMA_SETTINGS)
+    db.persist()
+    db = None
 
 def main():
     for root, dirs, files in os.walk("docs"):
         for file in files:
             if file.endswith(".pdf"):
                 print(file)
-                loader = PyPDFLoader(os.path.join(root, file))
-    documents = loader.load()
-    print("splitting into chunks")
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-    texts = text_splitter.split_documents(documents)
-    #create embeddings here
-    print("Loading sentence transformers model")
-    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-    #create vector store here
-    print(f"Creating embeddings. May take some minutes...")
-    db = Chroma.from_documents(texts, embeddings, persist_directory=persist_directory, client_settings=CHROMA_SETTINGS)
-    db.persist()
-    db=None 
+                file_path = os.path.join(root, file)
+                with st.spinner(f'Embeddings for {file} are in process...'):
+                    data_ingestion(file_path)
+                st.success(f'Embeddings for {file} are created successfully!')
 
     print(f"Ingestion complete! You can now run privateGPT.py to query your documents")
 
